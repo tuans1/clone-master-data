fbebf1a5fd8b98941751e66396187d9e714ad499
nguyen.toan <nguyen.toan@ascendcorp.com>
2023-01-05 21:55:40 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import "./style/tailwind.css";
+import "./style/element-ui/main.scss";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(ElementPlus).use(router).mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="w-[256px]">
+    <el-row class="w-full">
+      <el-col>
+        <el-menu default-active="2" class="el-menu-vertical-demo">
+          <el-sub-menu index="1">
+            <template #title>
+              <el-icon></el-icon>
+              <span>Navigator One</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">item one</el-menu-item>
+              <el-menu-item index="1-2">item two</el-menu-item>
+            </el-menu-item-group>
+          </el-sub-menu>
+        </el-menu>
+      </el-col>
+    </el-row>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { reactive } from "vue";
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  setup() {
+    const state = reactive({
+      active: 0,
+    });
+    const toggleModalState = () => {
+      console.log(state);
+    };
+    return {
+      toggleModalState,
+    };
   },
+  components: {},
 };
 </script>
